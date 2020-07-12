444d21287fb7be50cb1532cf121e60d097adbbf1
Paul Olabisi <pauloolabisi@gmail.com>
2020-07-12 19:39:18 +0100

    added bare logic

--- TransactionFeeCalculator/Program.cs
+++ TransactionFeeCalculator/Program.cs
@@ -7,6 +7,21 @@ namespace TransactionFeeCalculator
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Welcome to the Transaction Fee Calculator");
+
+            Console.WriteLine("Please enter the amount you want to transfer");
+            decimal amountToBeTransferred = Convert.ToDecimal(Console.ReadLine());
+
+            decimal transactionFee = ComputeTransactionAmount(amountToBeTransferred);
+
+            Console.WriteLine($"The transaction fee is: {transactionFee}");
+
+            Console.ReadLine();
+        }
+
+        private static decimal ComputeTransactionAmount(decimal amountToBeTransferred)
+        {
+            return amountToBeTransferred;
         }
     }
 }
